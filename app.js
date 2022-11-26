354c5918bb70be9615d097f82d5bef01e2ffaa4e
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-11-26 22:20:26 +0100

    ___

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const searchBtn = document.querySelector("#search_btn");
 window.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
@@ -9,7 +11,6 @@ searchBtn.addEventListener("click", () => {
   const APIKey = "7b05c9bceece29a17e816f92cac7cda8";
   let city = document.querySelector("#city").value;
   if (city === null || city === "") {
-    console.log("invalid city");
     return;
   }
 
@@ -19,63 +20,66 @@ searchBtn.addEventListener("click", () => {
     .then((response) => response.json())
     .then((json) => {
       const container = document.querySelector(".result");
+      console.log(container);
       container.style.display = "block";
       container.style.animation = "fadeIn 1s ease-out forwards";
       const weatherInfo = document.querySelector(".weather-info");
       const temperature = document.getElementById("temperature");
       const humidity = document.querySelector(".humidity span");
       const wind = document.querySelector(".wind span");
-      let statImg = document.querySelector("#status-img");
-      let status = document.querySelector("#status-container");
+      let statImg = document.getElementById("status-img");
 
-      if (json.cod === "404") {
+      if (json.cod === "404" || json.cod[0] === "4") {
+        document.body.style.backgroundColor = "rgba(75, 145, 192, 0.9)";
+        hide_visibility(weatherInfo, temperature);
         statImg.src = "images/404.png";
-        weatherInfo.style.display = "none";
-        status.innerHTML = "";
-        return;
-      } else if (json.cod[0] === "4") {
-        console.log("an error happened Please try again");
-        weatherInfo.style.display = "none";
-        status.innerHTML = "";
         return;
       }
 
-      document.body.style.backgroundColor = "rgba(75, 145, 192, 0.75)";
+      reset_visibility(weatherInfo, temperature);
+      document.body.style.backgroundColor = "rgba(75, 145, 192, 0.8)";
       weatherInfo.style.display = "flex";
-      console.log(json);
-      console.log("--- getting weather ---");
       let description = json.weather[0].main;
-      switch (description) {
-        case "Clear":
-          statImg.src = "images/clear.png";
-          break;
-
-        case "Rain":
-          statImg.src = "images/rain.png";
-          break;
-
-        case "Snow":
-          statImg.src = "images/snow.png";
-          break;
-
-        case "Clouds":
-          statImg.src = "images/cloud.png";
-          break;
-
-        case "Haze":
-          statImg.src = "images/mist.png";
-          break;
-
-        case "Mist":
-          statImg.src = "images/mist.png";
-          break;
-
-        default:
-          statImg.src = "images/clear.png";
-      }
-
+      set_img(statImg, description);
       temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
       humidity.innerHTML = `${json.main.humidity}%`;
       wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+      console.log("all good");
     });
 });
+
+function set_img(statImg, description) {
+  switch (description) {
+    case "Clear":
+      statImg.src = "images/clear.png";
+      break;
+    case "Rain":
+      statImg.src = "images/rain.png";
+      break;
+    case "Snow":
+      statImg.src = "images/snow.png";
+      break;
+    case "Clouds":
+      statImg.src = "images/cloud.png";
+      break;
+    case "Haze":
+      statImg.src = "images/mist.png";
+      break;
+    case "Mist":
+      statImg.src = "images/mist.png";
+      break;
+    default:
+      statImg.src = "images/clear.png";
+  }
+}
+
+function hide_visibility(a, b) {
+  a.style.visibility = "hidden";
+  b.style.visibility = "hidden";
+}
+
+function reset_visibility(a, b) {
+  a.style.visibility = "visible";
+  b.style.visibility = "visible";
+}
